a5577a33ceb9081d238e78c97a971afdfee33a7c
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-07-08 10:39:35 +0300

    Update main.tf

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     admin_username = "ubuntu"
 
   ssh_key {
-      key_data = azapi_resource.ssh_public_key.properties.publicKey
+      key_data = azapi_resource_action.ssh_public_key_gen.output["publicKey"]
     }
   }
   
