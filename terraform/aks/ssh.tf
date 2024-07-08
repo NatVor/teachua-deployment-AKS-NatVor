3963d8f6c8cc5484efd10c51d3d9b405bf10ed87
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-07-08 11:09:32 +0300

    Update ssh.tf

--- terraform/aks/ssh.tf
+++ terraform/aks/ssh.tf
@@ -1,24 +1,22 @@
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
+provider "github" {
+  token = var.github_token
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey", "privateKey"]
+data "github_actions_secret" "ssh_public_key_secret" {
+  repository = "your/repository"
+  secret_name = "SSH_PUBLIC_KEY"
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
-  parent_id = azurerm_resource_group.rg.id
-}
+  type     = "Microsoft.Compute/sshPublicKeys"
+  name     = "sshgenuinerhino"
+  location = "East US"
 
-output "key_data" {
-  value = azapi_resource_action.ssh_public_key_gen.output["publicKey"]
+  body = jsonencode({
+    properties = {
+      publicKey = data.github_actions_secret.ssh_public_key_secret.value
+    }
+  })
+
+  resource_group_name = azurerm_resource_group.rg.name
 }
