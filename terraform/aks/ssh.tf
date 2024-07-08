be0da0cca5d9e941b650b1258cfbb447991f3803
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-07-08 10:38:55 +0300

    Update ssh.tf

--- terraform/aks/ssh.tf
+++ terraform/aks/ssh.tf
@@ -1,14 +1,24 @@
+resource "random_pet" "ssh_key_name" {
+  prefix    = "ssh"
+  separator = ""
+}
+
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id = azapi_resource.ssh_public_key.id
+  action      = "generateKeyPair"
+  method      = "POST"
+
+  response_export_values = ["publicKey", "privateKey"]
+}
+
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys"
-  name      = "sshgenuinerhino"
-  location  = "East US"
-  resource_group_name = azurerm_resource_group.rg.name
+  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  name      = random_pet.ssh_key_name.id
+  location  = azurerm_resource_group.rg.location
+  parent_id = azurerm_resource_group.rg.id
+}
 
-  body = jsonencode({
-    properties = {
-      publicKey = "ssh-rsa ..."
-    }
-  })
-
-  response_export_values = ["id", "name"]
+output "key_data" {
+  value = azapi_resource_action.ssh_public_key_gen.output["publicKey"]
 }
