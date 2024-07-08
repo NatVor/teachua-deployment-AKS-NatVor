e1b95c6a6e9df17687e8d798f4ec3031ef96a102
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-07-08 16:17:28 +0300

    Update ssh.tf

--- terraform/aks/ssh.tf
+++ terraform/aks/ssh.tf
@@ -1,22 +1,22 @@
-provider "github" {
-  token = var.github_token
-}
+#provider "github" {
+#  token = var.github_token
+#}
 
-data "github_actions_secret" "ssh_public_key_secret" {
-  repository = "teachua-deployment-AKS-NatVor"
-  secret_name = "SSH_PUBLIC_KEY"
-}
+#data "github_actions_secret" "ssh_public_key_secret" {
+# repository = "teachua-deployment-AKS-NatVor"
+# secret_name = "SSH_PUBLIC_KEY"
+#}
 
-resource "azapi_resource" "ssh_public_key" {
-  type     = "Microsoft.Compute/sshPublicKeys"
-  name     = "sshgenuinerhino"
-  location = "East US"
+#resource "azapi_resource" "ssh_public_key" {
+# type     = "Microsoft.Compute/sshPublicKeys"
+# name     = "sshgenuinerhino"
+# location = "East US"
 
-  body = jsonencode({
-    properties = {
-      publicKey = data.github_actions_secret.ssh_public_key_secret.value
-    }
-  })
+# body = jsonencode({
+#   properties = {
+#     publicKey = data.github_actions_secret.ssh_public_key_secret.value
+#   }
+# })
 
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource_group_name = azurerm_resource_group.rg.name
+#}
