c0689702de288aa5c2f913098457b88f8cf9b202
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-07-10 20:37:46 +0300

    Update variables.tf

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -16,41 +16,8 @@ variable "node_count" {
   default     = 3
 }
 
-#variable "msi_id" {
-#  type        = string
-#  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
-#  default     = null
-#}
-
 variable "username" {
   type        = string
   description = "The admin username for the new cluster."
   default     = "azureadmin"
 }
-
-variable "client_id" {
-  type = string
-  description = "The client ID (application ID) of the Azure AD application/service principal."
-}
-
-variable "client_secret" {
-  type = string
-  description = "The client secret (password) of the Azure AD application/service principal."
-}
-
-variable "subscription_id" {
-  type    = string
-  description = "Subscription ID for Azure"
-}
-
-variable "tenant_id" {
-  type    = string
-  description = "Tenant ID for Azure"
-}
-
-#variable "ssh_public_key" {
-#  default = "~/.ssh/TeachVM_key.pub"
-#  default = ""
-#}
-
-
